8f226c0c6c0b2c9ba62cf81d0523ac4e7de8680b
Tech <tech@abcs-MacBook-Air.local>
2026-04-01 07:02:47 -0700

    updating cors

--- server.js
+++ server.js
@@ -72,6 +72,8 @@ const allowedOrigins = [
   'http://localhost:8080',
   'https://frontend-belucky.ngrok-free.dev',
   'https://your-production-domain.com',
+  'https://sofiaslots-admin-frontend.onrender.com/',
+  'https://sofiaslots-frontend-user.onrender.com/',
 ];
 
 app.use((req, res, next) => {
